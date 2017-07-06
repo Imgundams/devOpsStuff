65be431095c5fdcab2e2dbef37f73289cc789bdc
Imgundams <Imgundams@users.noreply.github.com>
2017-07-07 04:05:11 +1200

    final

--- Documents/Vagrant Setup/bootstrap.sh
+++ Documents/Vagrant Setup/bootstrap.sh
@@ -20,12 +20,12 @@ echo "JAVA Installed"
 
 # SBT install
 
-# echo "SBT Installing"
-# echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-# sudo apt-get update
-# sudo apt-get install sbt
-# echo "SBT Installed"
+echo "SBT Installing"
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+sudo apt-get update
+sudo apt-get install sbt
+echo "SBT Installed"
 
 # Installing JIRA make sure the jira.bin is inside the folder below
 echo "JIRA Installing"
