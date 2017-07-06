40a4f8a2e46906b10431b6189b689e0502363a16
Imgundams <Imgundams@users.noreply.github.com>
2017-07-07 03:01:34 +1200

    end of Thursday push

--- Documents/Vagrant Setup/bootstrap.sh
+++ Documents/Vagrant Setup/bootstrap.sh
@@ -19,33 +19,45 @@ sudo apt install oracle-java8-set-default
 echo "JAVA Installed"
 
 # SBT install
-echo "SBT Installing"
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-sudo apt-get update
-sudo apt-get install sbt
-echo "SBT Installed"
+
+# echo "SBT Installing"
+# echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+# sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+# sudo apt-get update
+# sudo apt-get install sbt
+# echo "SBT Installed"
 
 # Installing JIRA make sure the jira.bin is inside the folder below
 echo "JIRA Installing"
-sudo chmod a+x /vagrant_data/Documents/jira.bin
-sudo printf 'o\n2\n\n\n2\n8081\n8006\ny\n' | ./vagrant_data/Documents/jira.bin
+sudo mv /vagrant_data/jira.bin /opt  
+cd /opt
+sudo chmod a+x jira.bin
+sudo printf 'o\n2\n\n\n2\n8082\n8007\ny\n' | ./jira.bin
 echo "JIRA Installed"
+
+# Setting up Nexus
+sudo mv /vagrant_data/nexus-3.0.2-02-unix.tar.gz /opt  
+cd /opt/
+sudo tar zxvf nexus-3.0.2-02-unix.tar.gz
+sudo mv nexus-3.0.2-02 nexus
+cd nexus/bin
+sudo RUN_AS_USER=root ./nexus start
 
 # Setting up Jenkins
 sudo wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -y jenkins
-sudo chmod 755 /var/lib/jenkins/secrets
-echo "password for initialAdminPassword is: "
-sudo cat "/var/lib/jenkins/secrets/initialAdminPassword" 
+sudo chmod -R 755 /var/lib/jenkins/secrets
+
 sudo su jenkins -s /bin/bash
-ssh-keygen
-printf"/vargrant_data/Documents/superSecret"
+echo "/vargrant_data/superSecret\n\n\n" | ssh-keygen
 sudo service jenkins start
+echo "vagrant" | su vagrant
+
 
  sudo apt-get -y autoremove
+ 
  # All code done now checking if software is installed
  echo "Displaying software versions..."
  apache2 -vS
@@ -55,4 +67,7 @@ sudo service jenkins start
  java -version
  javac -version
  sudo service jenkins status
+ ifconfig eth0
+ echo "password for initialAdminPassword is: "
+sudo cat "/var/lib/jenkins/secrets/initialAdminPassword" 
  echo "Finished script"
